35e5e7ff99cd7c99d7832875209696abc9fde277
André Benevides <andre.rhagnar93@gmail.com>
2020-11-14 14:37:32 +0000

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Navbar from './Navbar.svelte'
+	import Player from './Player.svelte'
+	import AddPlayer from './AddPlayer.svelte'
+
+	let players = [
+		{ name: "John Doe", points: 10 },  
+		{ name: "Jane Doe", points: 32 },  
+		{ name: "Oliver Doe", points: 120 },  
+	];
+
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		//players.push(newPlayer) não funciona porque os objectos no svelte são immutable (tal como no react)
+		players = [...players, newPlayer];
+	}
+
+	const removePlayer = (e) => {
+		players = players.filter(player => player.name !== e.detail);
+	}
+</script>
+
+<main>
+	<Navbar />
+	<AddPlayer on:addplayer={addPlayer}/>
+	{#if players.length === 0}
+		<p>There are no players available!</p>
+	{:else}
+		{#each players as player}
+			<Player  on:removeplayer={removePlayer} name={player.name} points={player.points} />
+		{/each}
+	{/if}
+</main>
